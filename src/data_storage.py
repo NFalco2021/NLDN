c3ae497cb802b37b966055a2d85b764bd6ac0d1a
alexisnagle <97808552+alexisnagle@users.noreply.github.com>
2022-04-22 11:38:57 -0400

    Remove unused read_json() and write_json() methods

--- src/data_storage.py
+++ src/data_storage.py
@@ -34,22 +34,6 @@ class DataStorage:
             except Exception as ex:
                 logging.exception(f"Exception {ex} occurred")
 
-    # Reads json file 'path' and returns it's data
-    def read_json(self, path):
-        with open(path, 'r+') as f:
-            try:
-                return json.load(f)
-            except Exception as ex:
-                logging.exception(f"Exception {ex} occurred")
-
-    # Writes 'data' to the specified 'path'
-    def write_json(self, path, data):
-        with open(path, 'w+') as f:
-            try:
-                json.dump(data, f)
-            except Exception as ex:
-                logging.exception(f"Exception {ex} occurred")
-
     # Getter Methods
     # Returns the filename the data is written to (ie. NLDN_formattedTime)
     def get_filename(self):
