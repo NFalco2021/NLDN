19421da604950aa781cbab96cb3e8ef2c4a4a662
alexisnagle <97808552+alexisnagle@users.noreply.github.com>
2022-04-25 12:16:16 -0400

    Update refactoring

--- src/api_functions.py
+++ src/api_functions.py
@@ -3,59 +3,62 @@ import string
 import json
 import base64
 
+from config import Config
 
-class ApiFunctions:
-    def __init__(self, auth_url, username, password, payload, headers):
-        self.auth_response = self.set_auth_response(auth_url, username, password, payload, headers)
-        # print(json.dumps(self.auth_response, indent=4))
 
-        self.access = self.set_access()
-        self.refresh = self.set_refresh()
+def main():
+    c = Config()
+    self.auth_response = self.set_auth_response(c.get_auth_url(), c.get_username(), c.get_password(), c.get_payload(), c.get_headers())
+    # print(json.dumps(self.auth_response, indent=4))
 
-        self.access_b64diff = set()
-        self.refresh_b64diff = set()
+    self.access = self.set_access()
+    self.refresh = self.set_refresh()
 
-        # access and refresh tokens are not standard b64
-        # self.access_decoded = base64.b64decode(self.access)
-        # self.refresh_decoded = base64.b64decode(self.refresh)
-        self.set_access_b64diff()
-        self.set_refresh_b64diff()
+    self.access_b64diff = set()
+    self.refresh_b64diff = set()
 
-        self.print_access_b64diff()
-        self.print_refresh_b64diff()
-        # {'.', '_', '-'} are the results.
-        # Typical B64 encoding does not use those characters
-        # I believe they are a replacement for '+', '/', and '=' but not in that order
-        # Some trial and error is needed to figure out which is which.
+    # access and refresh tokens are not standard b64
+    # self.access_decoded = base64.b64decode(self.access)
+    # self.refresh_decoded = base64.b64decode(self.refresh)
+    self.set_access_b64diff()
+    self.set_refresh_b64diff()
 
-    def set_auth_response(self, auth_url, username, password, payload, headers):
-        return requests.request("POST",
-                                auth_url,
-                                auth=requests.auth.HTTPBasicAuth(username, password),
-                                data=payload,
-                                headers=headers
-                                ).json()
+    self.print_access_b64diff()
+    self.print_refresh_b64diff()
+    # {'.', '_', '-'} are the results.
+    # Typical B64 encoding does not use those characters
+    # I believe they are a replacement for '+', '/', and '=' but not in that order
+    # Some trial and error is needed to figure out which is which.
 
-    def set_access(self):
-        return self.auth_response["access_token"]
+def set_auth_response(self, auth_url, username, password, payload, headers):
+    return requests.request("POST",
+                            auth_url,
+                            auth=requests.auth.HTTPBasicAuth(username, password),
+                            data=payload,
+                            headers=headers
+                            ).json()
+def set_access(self):
+    return self.auth_response["access_token"]
 
-    def set_refresh(self):
-        return self.auth_response["refresh_token"]
+def set_refresh(self):
+    return self.auth_response["refresh_token"]
 
-    def set_access_b64diff(self):
-        for letter in self.access:
-            if letter not in string.ascii_letters:
-                if letter not in string.digits:
-                    self.access_b64diff.add(letter)
+def set_access_b64diff(self):
+    for letter in self.access:
+        if letter not in string.ascii_letters:
+            if letter not in string.digits:
+                self.access_b64diff.add(letter)
 
-    def set_refresh_b64diff(self):
-        for letter in self.refresh:
-            if letter not in string.ascii_letters:
-                if letter not in string.digits:
-                    self.refresh_b64diff.add(letter)
+def set_refresh_b64diff(self):
+    for letter in self.refresh:
+        if letter not in string.ascii_letters:
+            if letter not in string.digits:
+                self.refresh_b64diff.add(letter)
 
-    def print_access_b64diff(self):
-        print(self.access_b64diff)
+def print_access_b64diff(self):
+    print(self.access_b64diff)
 
-    def print_refresh_b64diff(self):
-        print(self.refresh_b64diff)
+def print_refresh_b64diff(self):
+    print(self.refresh_b64diff)
+
+main()
