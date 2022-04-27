5574fcdf515999f089c1b29da7fd1901e2274509
James-gpg <jamespabisz@protonmail.com>
2022-04-27 11:13:09 -0400

    changed basedir = /home/santiago/Spring2022/CSE2410/NLDN/
    basedir = os.path.dirname(os.path.abspath(__file__))
    root_directory = basedir[:len(basedir)-3]

--- tests/unit/test_data.py
+++ tests/unit/test_data.py
@@ -1,9 +1,23 @@
 import unittest
+import pytest
+import config as c
+import data_storage as d
+import time_functions as t
 
+from src.data_storage import write_csv
+no_key_data = None
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_filename(self):
+        time_formatted = t.return_formatted_time()
+        filename_equivalent = f'NLDN_{time_formatted}.csv'
+        self.assertEqual(d.filename, filename_equivalent)
+
+    def test_csv_writer_index(self):
+        self.assertRaises(IndexError, write_csv())
+
+    def test_csv_writer_key(self):
+        self.assertRaises(KeyError, write_csv(no_key_data))
 
 
 if __name__ == '__main__':
